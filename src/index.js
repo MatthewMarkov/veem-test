c8eaf60707d72838d546354a557a4a68261f83a0
matthew <mathew.markov@gmail.com>
2021-06-08 22:20:49 +0300

    правка README файла

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import App from './App';
-import './fonts/OpenSans-Bold.ttf';
-import './fonts/OpenSans-Regular.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
